ce46e08725ad8540130401f8011b9c5c8b7de01f
viking1304 <viking1304@gmail.com>
2021-03-27 19:16:52 +0100

    Add TextSniper 1.5.0

---
+++ Casks/textsniper.rb
@@ -0,0 +1,11 @@
+cask "textsniper" do
+  version "1.5.0"
+  sha256 :no_check
+
+  url "https://textsniper.app/api/downloads/mac-latest"
+  name "TextSniper"
+  desc "Extract text from images and other digital documents in seconds"
+  homepage "https://textsniper.app/"
+
+  app "TextSniper.app"
+end
