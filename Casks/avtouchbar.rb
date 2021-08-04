c5a2ccf04ddc9d385228ce94d33f8fd5fae9c52a
viking1304 <viking1304@gmail.com>
2021-08-04 17:50:23 +0200

    Fix download link for AVTouchBar 3.0.6

--- Casks/avtouchbar.rb
+++ Casks/avtouchbar.rb
@@ -2,7 +2,7 @@ cask "avtouchbar" do
   version "3.0.6"
   sha256 "f7d02d0ff050ef42b077f0941ff9ab7e65a4e3ee43f60dcd17758f6733e18c87"
 
-  url "https://www.avtouchbar.com/avtouchbar-#{version.dots_to_hyphens}/"
+  url "https://www.avtouchbar.com/wp-content/uploads/2021/01/AVTouchBar-3.0.6.zip"
   name "AVTouchBar"
   desc "Audio Visualizer for the Touch Bar"
   homepage "https://www.avtouchbar.com/"
