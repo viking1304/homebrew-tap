322185a45739899a4127d1c7d5cb8bfee5f22a3a
viking1304 <viking1304@gmail.com>
2021-08-19 18:01:41 +0200

    Add VidJuice UniTube 3.7.5

---
+++ Casks/vidjuice-unitube.rb
@@ -0,0 +1,11 @@
+cask "vidjuice-unitube" do
+  version "3.7.5"
+  sha256 :no_check
+
+  url "https://download.vidjuice.com/unitube.dmg", verified: "https://download.vidjuice.com/"
+  name "VidJuice UniTube"
+  desc "Download videos and audios from 10,000+ sites across all your devices"
+  homepage "https://www.vidjuice.com/"
+
+  app "VidJuice UniTube.app"
+end
